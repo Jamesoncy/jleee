c85c1e2b13fbe53545f1f7b8007a08d9a922b681
JamesRoncy <JamesRoncy13@gmail.com>
2016-01-12 12:31:49 -0800

    database

--- include/dbconnection.php
+++ include/dbconnection.php
@@ -1,5 +1,5 @@
 <?php  
-	$conn = mysql_connect('localhost', 'root', '');
+	$conn = mysql_connect('mysql15.000webhost.com', 'a7869948_reserve', 'sxapulkapreh123','a7869948_reserve');
 	 if (!$conn)
     {
 	 die('Could not connect: ' . mysql_error());
