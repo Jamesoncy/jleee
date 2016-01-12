263f7b4be8618255fa5f4611b3997ed3735b1f37
JamesRoncy <JamesRoncy13@gmail.com>
2016-01-12 12:40:07 -0800

    dsada

--- include/dbconnection.php
+++ include/dbconnection.php
@@ -1,8 +1,8 @@
 <?php  
-	$conn = mysql_connect('mysql15.000webhost.com', 'a7869948_reserve', 'sxapulkapreh123','a7869948_reserve');
+	$conn = mysql_connect('http://sql10.000webhost.com/', 'a7869948_reserve', 'sxapulkapreh123');
 	 if (!$conn)
     {
 	 die('Could not connect: ' . mysql_error());
 	}
-	mysql_select_db("reservation", $conn);
+	mysql_select_db("a7869948_reserve", $conn);
 ?>
