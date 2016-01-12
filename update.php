946f2b75fd51089e582645e49968a74bd7eeb262
JamesRoncy <JamesRoncy13@gmail.com>
2016-01-12 11:49:04 -0800

    testing

--- update.php
+++ update.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\
 <?php 
-session_start();
-include("include/dbconnection.php"); ?>
+//session_start();
+//include("include/dbconnection.php"); ?>
 <html lang="en-US">
    <head>
       <meta charset="UTF-8">
